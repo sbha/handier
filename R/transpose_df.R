1fc3047b13309dc8d1fdfbfe842d50754ef166f0
“sbha” <sbharty@gmail.com>
2020-02-16 15:10:48 -0600

    nse option for dataframes without meaningful rownames

--- R/transpose_df.R
+++ R/transpose_df.R
@@ -1,8 +1,27 @@
-transpose_df <- function(x){
-  suppressWarnings(
-    x %>% 
-      rownames_to_column %>% 
-      gather(var, value, -rowname) %>% 
-      spread(rowname, value)
-  )
+transpose_df <- function(x, r = NULL){# , v = 'var'
+  #var <- enquo(v)
+  
+  if (is.null(r)) {
+    
+    suppressWarnings(
+      x %>% 
+        rownames_to_column() %>% 
+        gather(var, value, -rowname) %>% 
+        spread(rowname, value) #%>% filter(var != !!var) 
+    ) 
+    
+  } else if (!is.null(r)) {
+    r <- enquo(r)
+  
+    suppressWarnings(
+      x %>%
+        gather(var, value, -!!r) %>%
+        spread(!!r, value) #%>% filter(var != !!var)
+    )
+    
+  }
+  
 }
+
+
+
