6b77d2dbd56782811ce2205ba72328efcc313ac4
“sbha” <sbharty@gmail.com>
2021-08-22 19:17:04 -0500

    clean up

--- R/dt_to_md.R
+++ R/dt_to_md.R
@@ -3,14 +3,7 @@ df_to_md <- function(x){
   cat(
     paste('|', paste(names(x), collapse = ' | '), '|\n'),
     paste0('|', paste(rep('---|', ncol(x)), collapse = ''), '\n'),
-    x %>% 
-      #as_tibble() %>% 
-      #slice(1:5) %>% 
-      # mutate(md_start = '|') %>% 
-      # mutate(md_end = '|') %>% 
-      # unite(md_middle, everything(), sep = ' | ') %>% 
-      # select(md_start, md_middle, md_end) %>% 
-      # unite(md, md_start:md_end, sep = ' ') %>% 
+    x  %>% 
       unite(md_middle, everything(), sep = ' | ') %>% 
       mutate(md_start = '|') %>% 
       mutate(md_end = '|') %>% 
