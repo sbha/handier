52a4dea22723e5e7d635fbe565524214d99d5931
sbha <sbharty@gmail.com>
2024-04-06 13:24:14 -0500

    option to include cumulative sum

--- R/calculate_table_percentage.R
+++ R/calculate_table_percentage.R
@@ -12,21 +12,43 @@ calculate_table_percentage <- function(colname, as_decimal = FALSE){
 # starwars %>% count(species, sort = TRUE) %>% mutate(pct = calculate_table_percentage(n))
 
 
-ctp <- function(df, colname = 'n', as_decimal = FALSE, pct_name = 'pct'){
+ctp <- function(df, 
+                colname = 'n', 
+                as_decimal = FALSE, 
+                pct_name = 'pct', 
+                include_cumsum = FALSE){
   
   df[[pct_name]] <- df[[colname]] / sum(df[[colname]])
   
-  if (as_decimal == TRUE) {
+  if (include_cumsum){
+    
+    df <- df %>% 
+      mutate(csum = cumsum(df[[pct_name]]))
+    
+  }
+  
+  if (as_decimal) {
+    
     df
+    
   } else {
+    
     df[[pct_name]] <- df[[pct_name]] * 100
+    
+    if (include_cumsum){
+      
+      df <- df %>% 
+        mutate(csum = csum * 100)
+      
+    }
+    
     df
   }
 }
 
-# starwars %>% 
-#   count(species, sort = TRUE) %>%
-#   ctp
+starwars %>%
+  count(species, sort = TRUE) %>%
+  ctp
 # 
 # starwars %>% 
 #   count(species, sort = TRUE) %>%
@@ -37,3 +59,7 @@ ctp <- function(df, colname = 'n', as_decimal = FALSE, pct_name = 'pct'){
 #   ctp('cnt', TRUE, 'percentage')
 
 # https://stackoverflow.com/a/36015931/3058123
+
+# starwars %>%
+#   count(species, sort = TRUE) %>%
+#   ctp(., include_cumsum = TRUE)
