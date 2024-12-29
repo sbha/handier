0a05faa47e1d658ffe6e8973b9e010b518f2de3d
sbha <sbharty@gmail.com>
2024-12-28 20:28:24 -0600

    argument name

--- R/rename_file.R
+++ R/rename_file.R
@@ -7,11 +7,11 @@ rename_file <- function(file_name, to_replace, replace_with){
 # list.files('./dirpath/', '.R', full.names = TRUE) %>% 
 #   map(~rename_file(., '_shortname_', '_longname_'))
 
-rename_dir_files <- function(dir, re_matching = '*', to_replace, to_replace){
-  
-  list.files(dir, re_matching, full.names = TRUE) %>% 
-    map(~rename_file(., to_replace, to_replace))
-  
+rename_dir_files <- function(dir, re_matching = '*', to_replace, replace_with){
+
+  list.files(dir, re_matching, full.names = TRUE) %>%
+    map(~rename_file(., to_replace, replace_with))
+
 }
 
 
