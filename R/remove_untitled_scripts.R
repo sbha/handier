870bd32e9deb98651d1d44819d7f7c03406ff975
sbha <sbharty@gmail.com>
2026-01-31 05:07:43 -0600

    message updates

--- R/remove_untitled_scripts.R
+++ R/remove_untitled_scripts.R
@@ -49,7 +49,7 @@ remove_untitled_scripts <- function(
   if (prompt_interactive && base::interactive()){
     response <- menu(
       c("No", "Yes"), 
-      title=glue::glue("Do you want to delete the following untitled scripts?\n {df_untitled}")
+      title=glue::glue("Do you want to delete the following untitled scripts?\n {df_ut$untitled_file}")
       )
     if (response == 2){
       delete_files <- TRUE
@@ -62,7 +62,7 @@ remove_untitled_scripts <- function(
     df_untitled %>% 
       map(file.remove)
   } else {
-    message('delete files set to FALSE. {nrow(df_untitled) not deleted}')
+    message('delete files set to FALSE. {nrow(df_untitled)} files not deleted')
     df_untitled
   }
 }
